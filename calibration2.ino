c649c51bf03840165234a1cd87e2fd46db9ae5df
junrong-jr <56990597+junrong-jr@users.noreply.github.com>
2020-09-04 10:44:55 +0800

    latest calibrate

---
+++ calibration2.ino
@@ -0,0 +1,33 @@
+/********************************************************
+ * PID Basic Example
+ * Reading analog input 0 to control analog PWM output 3
+ ********************************************************/
+
+#include <EnableInterrupt.h>
+#include <PID_v1.h> 
+#include "DualVNH5019MotorShield.h"
+//Motor 1
+#define encoder1A 3
+#define encoder1B 5
+
+//Motor 2
+#define encoder2A 11
+#define encoder2B 13
+DualVNH5019MotorShield motor;
+
+
+void setup()
+{
+  //initialize the variables we're linked to
+  Serial.begin(9600);
+  
+  setupPID();
+  setupEncoder();
+  initMove();
+}
+
+void loop()
+{  
+  calibration();
+}
+ 
